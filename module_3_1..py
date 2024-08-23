7b455e38bb83e271f9854497fc39ff1001243602
Sachvk <serg22011985@gmail.com>
2024-08-23 14:20:57 +0300

    Update module_3_1..py

--- module_3_1..py
+++ module_3_1..py
@@ -1,31 +1,23 @@
 calls = 0
 
 
-def count_calls(calls):
-    print(calls)
+def count_calls():
+    global calls
+    calls += 1
 
 
 def string_info(string):
-    global calls
-    calls += 1
-    a = (len(string), string.upper(), string.lower())
-    return a
+    count_calls()
+    return(len(string), string.upper(), string.lower())
+
 
 
 def is_contains(string, list_to_search):
-    global calls
-    calls += 1
-    for item in list_to_search:
-        b = []
-        b.append(item.lower())
-    if string.lower() in b:
-        return True
-    else:
-        return False
-
+    count_calls()
+    return string.lower() in (item.lower() for item in list_to_search)
 
 print(string_info('Flower'))
 print(string_info('Wind'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
-print(is_contains('cycle', ['recycling', 'cyclic']))
+print(is_contains('Urban', ['ban', 'BaNaN']))
+print(is_contains('cycle', ['recycling', 'cyclic', 'CYclE']))
 print(calls)
